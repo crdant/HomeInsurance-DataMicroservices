5b4768bcaa8004959b8873f2a29ab72c90696774
Chuck D'Antonio <c_dantonio@acm.org>
2016-10-17 21:21:51 -0400

    Added full paths to CSV files

--- scripts/model_postgres.sql
+++ scripts/model_postgres.sql
@@ -1,12 +1,12 @@
 drop table if exists POS_DEVICE cascade;
-create table POS_DEVICE (	
+create table POS_DEVICE (
 	id	bigint ,
 	location text,
 	merchant_name	text
 );
 
 drop table if exists TRANSACTION cascade;
-create table TRANSACTION (		
+create table TRANSACTION (
 	id bigint,
 	device_id	 bigint,
 	transaction_value decimal(10,2),
@@ -15,11 +15,10 @@ create table TRANSACTION (
 );
 
 drop table if exists ZIP_CODES cascade;
-CREATE TABLE ZIP_CODES 
-	(ZIP char(5), LATITUDE double precision, LONGITUDE double precision, 
+CREATE TABLE ZIP_CODES
+	(ZIP char(5), LATITUDE double precision, LONGITUDE double precision,
 	CITY varchar, STATE char(2), COUNTY varchar, NAME varchar);
 
-COPY transaction FROM 'transaction.csv' DELIMITER ',' CSV HEADER;	
-COPY zip_codes FROM 'zip_codes_states.csv' DELIMITER ',' CSV HEADER;
-COPY pos_device FROM 'pos_device.csv' DELIMITER ',' CSV HEADER;
-
+COPY transaction FROM '/Users/crdant/Source/demos/FraudDetection-DataMicroservices/scripts/transaction.csv' DELIMITER ',' CSV HEADER;
+COPY zip_codes FROM '/Users/crdant/Source/demos/FraudDetection-DataMicroservices/scripts/zip_codes_states.csv' DELIMITER ',' CSV HEADER;
+COPY pos_device FROM '/Users/crdant/Source/demos/FraudDetection-DataMicroservices/scripts/pos_device.csv' DELIMITER ',' CSV HEADER;
