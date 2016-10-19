6f3c2fdb9c0eda99c6165cbd0832a33bd3001e55
Chuck D'Antonio <c_dantonio@acm.org>
2016-10-19 13:44:42 -0400

    Quoted some arguments to resolve command-line parsing issues

--- register-to-scdf-pcfdev.sh
+++ register-to-scdf-pcfdev.sh
@@ -1,3 +1,4 @@
 export SCDF_BUILD_PATH=/Users/crdant/Source/spring-cloud-dataflow
+export DEMO_PATH=/Users/crdant/Source/demos/HomeInsurance-DataMicroservices
 
-java -Djavax.net.ssl.trustStorePassword=dataflow -Djavax.net.ssl.trustStore=dataflow.truststore -jar $SCDF_BUILD_PATH/spring-cloud-dataflow-shell-1.0.1.RELEASE.jar --dataflow.uri=https://dataflow-server.local.pcfdev.io --spring.shell.commandFile=scripts/streams-shell.txt
+java -Djavax.net.ssl.trustStorePassword=dataflow -Djavax.net.ssl.trustStore=dataflow.truststore -jar $SCDF_BUILD_PATH/spring-cloud-dataflow-shell-1.0.1.RELEASE.jar --dataflow.uri="https://dataflow-server.local.pcfdev.io" --spring.shell.commandFile="scripts/streams-shell.txt"
