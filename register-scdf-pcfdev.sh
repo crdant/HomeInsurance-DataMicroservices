c2205d0cb8e23fb9d4d5d4692c911b55d31aa2c0
Chuck D'Antonio <c_dantonio@acm.org>
2016-10-19 11:30:19 -0400

    Added script to automate the setup of SCDF pipelines and streams

---
+++ register-scdf-pcfdev.sh
@@ -0,0 +1,3 @@
+export SCDF_BUILD_PATH=/Users/crdant/Source/spring-cloud-dataflow
+
+java -Djavax.net.ssl.trustStorePassword=dataflow -Djavax.net.ssl.trustStore=dataflow.truststore -jar $SCDF_BUILD_PATH/spring-cloud-dataflow-shell-1.0.1.RELEASE.jar --dataflow.uri=https://dataflow-server.local.pcfdev.io --spring.shell.commandFile=scripts/streams-shell.txt
